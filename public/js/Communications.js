0798784ca13e7c07e66f9dde25da1b01f6e6a971
Anthony <anthonysiu@gmail.com>
2015-11-08 09:34:21 -0600

    wip

--- public/js/Communications.js
+++ public/js/Communications.js
@@ -3,8 +3,8 @@ function Communications(){
 }
 Communications.prototype = {
 	init: function(){
-		this.socket = io.connect('http://localhost:90/');
-		//this.socket = io.connect('http://104.197.217.162:8080/');
+		this.socket = io.connect('http://localhost:80/');
+		//this.socket = io.connect('http://104.197.217.162:80/');
 		this.socket.on('chat', onChat);
 		this.socket.on('spawn', onSpawn);
 
